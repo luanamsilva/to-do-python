c9dfc3206d5fcfbf8b3df7379a33ba50f0c42d64
luanamsilva <luanamoura2002@yahoo.com.br>
2024-02-01 16:35:57 -0300

    created-crud

--- app.py
+++ app.py
@@ -1,6 +1,20 @@
-from flask import Flask
-
+from flask import Flask, request, jsonify
+from models.task import Task
 app = Flask(__name__)
+
+tasks = []
+task_id = 1
+
+@app.route('/tasks', methods=['POST'])
+def create_task():
+  global task_id
+  data = request.get_json()
+  newTask = Task(id=task_id,title=data.get("title") )
+  task_id+=1
+  tasks.append(newTask)
+  print(tasks)
+  return jsonify({"message": "Nova tarefa criada com sucesso."})
+
 
 if __name__ == "__main__":
   app.run(debug=True)

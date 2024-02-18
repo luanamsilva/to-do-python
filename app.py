2a0031ed9058e2f17296d70882da856688f0dc4d
luanamsilva <luanamoura2002@yahoo.com.br>
2024-02-18 18:41:16 -0300

    config-pylint

--- app.py
+++ app.py
@@ -3,33 +3,43 @@ from models.task import Task
 app = Flask(__name__)
 
 tasks = []
-task_id = 1
+TASK_ID = 1
 
 @app.route('/tasks', methods=['POST'])
 def create_task():
-  global task_id
-  data = request.get_json()
-  newTask = Task(id=task_id,title=data.get("title") )
-  task_id+=1
-  tasks.append(newTask)
-  print(tasks)
-  return jsonify({"message": "Nova tarefa criada com sucesso."})
+    global TASK_ID
+    data = request.get_json()
+    new_task = Task(id=TASK_ID,title=data.get("title") )
+    TASK_ID+=1
+    tasks.append(new_task)
+    print(tasks)
+    return jsonify({"message": "Nova tarefa criada com sucesso."})
 
 @app.route('/tasks', methods=['GET'])
 def get_tasks():
- task_list = [task.informations() for task in tasks]
- tasks_output = {
+    task_list = [task.informations() for task in tasks]
+    tasks_output = {
    "tasks": task_list,
    "total_tasks": len(task_list)
  }
- return jsonify(tasks_output)
+    return jsonify(tasks_output)
+
 
 @app.route('/tasks/<int:id>', methods=['GET'])
 def get_task(id):
-  for t in tasks:
-    if t.id == id:
-      return jsonify(t.informations())
-  return jsonify({'message':'Tarefa não encontrada'}, 404)
+    for t in tasks:
+        if t.id == id:
+            return jsonify(t.informations())
+    return jsonify({'message':'Tarefa não encontrada'}, 404)
+
+@app.route('/tasks/<int:id>', methods=['PUT'])
+def update_task(id):
+    task_update = None
+    for t in tasks:
+        if t.id == id:
+            task_update = t
+    if task_update is None:
+        return jsonify({"message": "Tarefa não encontrada"}, 404)
 
 if __name__ == "__main__":
-  app.run(debug=True)
+    app.run(debug=True)
--- models/task.py
+++ models/task.py
@@ -1,12 +1,12 @@
 class Task:
-  def __init__(self, id, title, done=False) -> None:
-    self.id = id
-    self.title = title
-    self.done = done
-  
-  def informations(self):
-    return{
-    "id": self.id,
-    "title": self.title,
-    "done": self.done
-    }
+    def __init__(self, id, title, done=False) -> None:
+        self.id = id
+        self.title = title
+        self.done = done
+
+    def informations(self):
+        return{
+        "id": self.id,
+        "title": self.title,
+        "done": self.done
+}
